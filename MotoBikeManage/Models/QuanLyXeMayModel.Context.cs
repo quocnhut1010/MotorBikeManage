1f2582db23a82df144b0d67adb92ad6800d81f6b
quocnhut1010 <128989079+quocnhut1010@users.noreply.github.com>
2025-03-23 21:18:44 +0700

    commit1

--- MotoBikeManage/Models/QuanLyXeMayModel.Context.cs
+++ MotoBikeManage/Models/QuanLyXeMayModel.Context.cs
@@ -31,6 +31,7 @@ namespace MotoBikeManage.Models
         public virtual DbSet<Import_Stock> Import_Stock { get; set; }
         public virtual DbSet<Maintenance> Maintenances { get; set; }
         public virtual DbSet<Supplier> Suppliers { get; set; }
+        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Vehicle> Vehicles { get; set; }
         public virtual DbSet<Inventory> Inventories { get; set; }
