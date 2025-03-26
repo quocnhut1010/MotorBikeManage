6e1685656cab4c4f24d85913d8d2c71cdf872823
namngxthanh <nam.ngx163@gmail.com>
2025-03-26 12:39:40 +0700

    testing push code

--- MotoBikeManage/Models/QuanLyXeMayModel.Designer.cs
+++ MotoBikeManage/Models/QuanLyXeMayModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\DoAnCN\MotoBikeManage\MotoBikeManage\Models\QuanLyXeMayModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Admin\source\repos\quocnhut1010\MotorBikeManage\MotoBikeManage\Models\QuanLyXeMayModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
